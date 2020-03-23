88bb5d3f4512107d03d3bc33f218453055ba39da
Ashish Sangale <aashishsangale07@gmail.com>
2020-03-23 15:54:26 +0530

    initial routes

--- app.js
+++ app.js
@@ -9,8 +9,31 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res){
+//Schema
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+})
 
+// model config
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+//Routes
+app.get('/', function(req, res){
+    res.redirect("/blogs")
+});
+
+app.get("/blogs", function(req,res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log(err)
+        } else {
+            res.render("index", {blogs:blogs});
+        }
+    })
 });
 
 
